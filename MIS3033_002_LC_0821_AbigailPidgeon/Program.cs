1d024a481738afbe11213eb2b0eac143370a7e64
pidg0000 <pidg0000@PCB-LAB360-34>
2023-08-22 19:40:36 -0500

    Add project files.

---
+++ MIS3033_002_LC_0821_AbigailPidgeon/Program.cs
@@ -0,0 +1,59 @@
+﻿// MIS 3033 002
+// August 21, 2023
+// Abigail Pidgeon 112515288
+
+// B:
+// C: C++: C++++ (C#, to compete with Java)
+// JavaScript ()
+// Python (you need to learn) 
+// statement, one line of code,
+Console.WriteLine("Welcome to MIS 3033!");
+//Console.writeLine("Welcome to MIS 3033!");
+// {}
+
+// comments 
+// single line comments ;
+/*
+ comment 1
+comment 2 
+ */
+
+// variables
+// data computation 
+// data: memory 16gb
+// computation: CPU 
+// variables to getspace in the memory 
+// expression: 12+24
+
+// variables: simple and complex (very important)
+// = assignment (very important)
+// simple: int double bool char ()
+// otherwise it is complex 
+
+// rules to define a variables
+// 3 rules to define a variable name:
+// 1. letters numbers _
+// 2. the first character is not a number 
+
+// datatype variable_name;
+int age;// datatype interger, variable name is age simple 
+double grade;// 
+bool isA;
+char lettergrade;
+
+// = assignment operator
+age = 20+12;// simple assignment, not expensive 
+            //
+
+// string, complex
+string str1;// complex, expensive 
+// not string space in the computer memory 
+str1=new string("Abigail Pidgeon");// new datatype()
+// complex assignment : copy the address or room number from the right to left 
+
+double r1;// simple 
+r1 = 2 + 3 * 4;// 24 or 14
+Console.WriteLine(r1);
+
+
+
